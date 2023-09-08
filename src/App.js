e5666d8774b25cefac65b3d00bf2c6e01ca47005
zxSHA Shi,Xiangyu (BI X) EXTERNAL <xiangyu.shi.ext@boehringer-ingelheim.com>
2023-09-08 09:28:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { truck } from './data.js';
 import './App.css';
+let Bmap = window.BMapGL;
+let pushCarts = truck.filter(truck =>
+  truck.FacilityType === 'PushCart' && truck.Longitude !== '0' && truck.Latitude !== '0'
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  size = 13;
+  componentDidMount() {
+    //initialize Baidu Map
+    this.map = new Bmap.Map("bmap");
+    let point = {
+      Longitude: 121.451754,
+      Latitude: 31.22985,
+      Applicant: '静安寺',
+      FacilityType: 'circle',
+    };
+    this.drawMap(point);
+  }
+
+  /**
+   * draw round markers based on lnt and lat
+   * @param {v} v an object that includes useful properties such as lnt, lat and Applicant so on.
+   */
+  drawMap = (v) => {
+    let point = new Bmap.Point(v.Longitude, v.Latitude);
+    let label = new Bmap.Label(`<div class=${v.FacilityType}>${v.Applicant}</div>`, {
+      offset: new Bmap.Size(0, 0),
+      position: point,
+    });
+    label.setStyle({
+      backgroundColor: "transparent",
+      border: "none",
+    });
+    if (v.Applicant === '静安寺') {
+      label.addEventListener("click", () => {
+        this.map.clearOverlays();
+        this.size += 6;
+        pushCarts.map((truck, index) => {
+          if (index <= 9) {
+            this.drawMap(truck);
+          }
+        });
+      });
+    } else {
+      label.addEventListener("click", () => {
+        alert(`This food truck sells: ${v.FoodItems === '' ? 'nothing' : v.FoodItems}:)`);
+      });
+    }
+    this.map.addOverlay(label);
+    this.map.centerAndZoom(point, this.size);
+  }
+
+  render() {
+    return (
+      <div id="bmap" style={{ height: '100vh' }}>App</div>
+    )
+  }
 }
-
-export default App;
